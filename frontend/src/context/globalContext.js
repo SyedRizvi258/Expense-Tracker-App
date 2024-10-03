26a696d867650556d7a99cb923ec978e946e090b
syedr <email_address>
2024-10-03 01:53:54 -0400

    Update API URL to use environment variable

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api/v1/';
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const GlobalContext = React.createContext();
 
